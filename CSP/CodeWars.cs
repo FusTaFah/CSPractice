3d00674dfc9124e4fce4e9038b51ad685d8deb69
FusTaFah <mfadel_1990@hotmail.com>
2025-04-24 20:53:30 +0100

    initial commit of all training exercises. First of many

---
+++ CSP/CodeWars.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CSP
+{
+    public static class EigthKyu
+    {
+        public static int Opposite(int number) => -number;
+
+        public static string ToAlternatingCase(this string s)
+        {
+            StringBuilder sb = new StringBuilder(s.Length);
+            foreach (Char c in s)
+            {
+                if (Char.IsLower(c))
+                {
+                    sb.Append(Char.ToUpper(c));
+                }
+                else if (Char.IsUpper(c))
+                {
+                    sb.Append(Char.ToLower(c));
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static string FakeBin(string x) =>
+            string.Concat(x.Select(c => int.Parse(c.ToString()) < 5 ? "0" : "1"));
+
+        public static int OtherAngle(int a, int b) => 180 - a - b;
+
+        public static string NumberToString(int num) => num.ToString();
+    }
+
+    public static class SeventhKyu
+    {
+        public static int[] Process2Arrays(int[] arr1, int[] arr2) =>
+            new int[] {
+                arr1.Intersect(arr2).Count() ,
+                arr1.Except(arr2).Concat(arr2.Except(arr1)).Count(),
+                arr1.Except(arr2).Count(),
+                arr2.Except(arr1).Count()
+            };
+
+        public static string MakeComplement(string dna) => string.Concat(dna.Select(x => 
+            x switch {
+                'A' => "T",
+                'T' => "A",
+                'C' => "G",
+                'G' => "C",
+                _ => string.Empty
+            }));
+
+        public static bool ValidatePin(string pin)
+        {
+            return Regex.IsMatch(pin, @"\d{4}") || Regex.IsMatch(pin, @"\d{6}");
+        }
+    }
+}
---
+++ CSPTest/ExerciseTests.cs
@@ -0,0 +1,128 @@
+using CSP;
+
+namespace CSPTest
+{
+    public class ExerciseTests
+    {
+        [Theory]
+        [InlineData(1, -1)]
+        [InlineData(-1, 1)]
+        public void Opposite(int number, int answer)
+        {
+            int retVal = EigthKyu.Opposite(number);
+            Assert.Equal(answer, retVal);
+        }
+
+        [Theory]
+        [InlineData("hello world", "HELLO WORLD")]
+        [InlineData("HELLO WORLD", "hello world")]
+        [InlineData("hello WORLD", "HELLO world")]
+        [InlineData("HeLLo WoRLD", "hEllO wOrld")]
+        [InlineData("12345", "12345")]
+        [InlineData("1a2b3c4d5e", "1A2B3C4D5E")]
+        [InlineData("String.ToAlternatingCase", "sTRING.tOaLTERNATINGcASE")]
+        public void ToAlternatingCase(string input, string answer)
+        {
+            string ans = input.ToAlternatingCase();
+            Assert.Equal(answer, ans);
+        }
+
+        [Theory]
+        [InlineData(
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+            new int[] {2, 4, 6, 8, 10, 12, 14}, 
+            new int[] { 4, 8, 5, 3 })]
+        [InlineData(
+            new int[] { 33, 2, 3, 37, 38, 40, 12, 10, 43, 44, 47, 49, 8, 19, 22, 24, 26, 28, 29, 30 },
+            new int[] { 1, 34, 17, 7, 9, 10, 43, 49, 22, 27, 28 },
+            new int[] { 5, 21, 15, 6 })]
+        [InlineData(
+            new int[] { 32, 34, 3, 4, 39, 10, 43, 13, 11, 18, 21, 22, 7, 25, 26, 36 },
+            new int[] { 32, 5, 38, 8, 41, 42, 12, 48, 40, 21, 22, 26, 10, 30 },
+            new int[] { 5, 20, 11, 9 })]
+        [InlineData(
+            new int[] { 0, 33, 37, 6, 10, 44, 13, 47, 16, 18, 22, 25 },
+            new int[] { 1, 38, 48, 8, 41, 7, 12, 47, 16, 40, 20, 23, 25 },
+            new int[] { 3, 19, 9, 10 })]
+        [InlineData(
+            new int[] { 0, 19, 34, 35, 5, 7, 45, 13, 3, 20, 26, 29, 30 },
+            new int[] { 33, 4, 38, 1, 8, 13, 47, 23, 28, 30, 31 },
+            new int[] { 2, 20, 11, 9 })]
+        [InlineData(
+            new int[] { 0, 35, 17, 6, 7, 10, 11, 46, 47, 16, 49, 20, 14, 23, 25, 26, 29 },
+            new int[] { 0, 6, 17, 42, 43, 47, 16, 49, 50, 21, 28, 30 },
+            new int[] { 6, 17, 11, 6 })]
+        public void Process2Arrays(int[] arr1, int[] arr2, int[] answer)
+        {
+            int[] ans = SeventhKyu.Process2Arrays(arr1, arr2);
+            Assert.Equal(answer, ans);
+        }
+
+        [Theory]
+        [InlineData("45385593107843568", "01011110001100111")]
+        [InlineData("509321967506747", "101000111101101")]
+        [InlineData("366058562030849490134388085", "011011110000101010000011011")]
+        public void FakeBin(string input, string answer)
+        {
+            string ans = EigthKyu.FakeBin(input);
+            Assert.Equal(ans, answer);
+        }
+
+        [Theory]
+        [InlineData(30, 60, 90)]
+        [InlineData(60, 60, 60)]
+        [InlineData(43, 78, 59)]
+        [InlineData(10, 20, 150)]
+        public void OtherAngle(int a, int b, int answer)
+        {
+            Assert.Equal(answer, EigthKyu.OtherAngle(a, b));
+        }
+
+        [Theory]
+        [InlineData(67, "67")]
+        [InlineData(79585, "79585")]
+        [InlineData(1 + 2, "3")]
+        [InlineData(1 - 2, "-1")]
+        [InlineData(0, "0")]
+        public void NumberToString(int number, string answer)
+        {
+            string ans = EigthKyu.NumberToString(number);
+            Assert.Equal(answer, ans);
+        }
+
+        [Theory]
+        [InlineData("AAAA", "TTTT")]
+        [InlineData("GTAT", "CATA")]
+        [InlineData("AAGG", "TTCC")]
+        [InlineData("CGCG", "GCGC")]
+        [InlineData("ATTGC", "TAACG")]
+        [InlineData("GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA", "CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT")]
+        public void MakeComplement(string input, string answer)
+        {
+            string ans = SeventhKyu.MakeComplement(input);
+            Assert.Equal(ans, answer);
+        }
+
+        [Theory]
+        [InlineData("1", false)]
+        [InlineData("12", false)]
+        [InlineData("123", false)]
+        [InlineData("12345", false)]
+        [InlineData("1234567", false)]
+        [InlineData("-1234", false)]
+        [InlineData("1.234", false)]
+        [InlineData("-1.234", false)]
+        [InlineData("00000000", false)]
+        [InlineData("1234", true)]
+        [InlineData("0000", true)]
+        [InlineData("1111", true)]
+        [InlineData("123456", true)]
+        [InlineData("098765", true)]
+        [InlineData("000000", true)]
+        [InlineData("090909", true)]
+        public void ValidatePin(string pin, bool answer)
+        {
+            Assert.Equal(answer, SeventhKyu.ValidatePin(pin));
+        }
+    }
+}
